b48d7e0cb1e59cf2e9c678ce58361fa2adb478c9
JotaN <jndomingos1998@gmail.com>
2019-01-07 19:17:34 +0000

    Caso introdução booleana

---
+++ type.ml
@@ -0,0 +1,14 @@
+open Ast 
+
+exception Type_Error of string
+
+type types =
+  Int
+
+let check_cst = function
+  | Cint _ -> initializer
+
+let rec type_expr cst = function
+  B
+  begin 
+    let e1 = type_exr cst 
